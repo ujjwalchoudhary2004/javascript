8db39a8ec9500cead8afc40a007f3f32ba2dcff7
ujjwalchoudhary2004 <9460ujjwaljanu@gmail.com>
2024-09-19 09:05:51 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,16 @@
+//alert(3+3)
+
+
+// number=> 2to power 53
+
+//bigInt
+//string=>""
+//boolean=> true/false
+
+//null=>standalone value
+//undefined
+//symbole=>unique
+//object
+let name="ujjwal";
+
+console.log(typeof hitesh);
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=14453
+let accountEmail="hitesh@gmail.com"
+var accountPassword="12434"
+accountCity="jaipur"
+
+accountEmail="huy@gmail.com"
+accountPassword="2434"
+accountCity="bangluru"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
