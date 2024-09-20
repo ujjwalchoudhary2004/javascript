d09a24c7b7973fc9c0f68350edc4061136a403ee
ujjwalchoudhary2004 <9460ujjwaljanu@gmail.com>
2024-09-20 06:09:15 +0000

    opertions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+/*let score=33
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+
+//let value=3;
+//let negValue= -value
+//console.log(negValue);
+console.log(2+2);
+console.log(2-2);
+console.log(2**2);
+console.log(2/2);
