b1c0fc6c6665594e2a30316ae1c585e3aed64e1a
ujjwalchoudhary2004 <9460ujjwaljanu@gmail.com>
2024-09-20 06:08:55 +0000

    datatypesummery

---
+++ datatypesummery.js
@@ -0,0 +1,23 @@
+const score=300;
+const outsideTemp=null
+
+let userEmail;
+
+const id= Symbol('123')
+const anotherId=Symbol('123')
+
+//console.log(id===anotherId);
+const bigNumber=12345567823557n
+
+
+const heros=["ujjwal","akash","abhishek"];
+let myobj={
+    name="hitesh"
+    age=21;
+
+}
+const myfunction = function()
+{
+    console.log("hello world");
+}
+console.log(typeof bigNumber);
